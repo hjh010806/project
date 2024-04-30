d327cf6fa4b92dda26b8a54be284c0686fa619f1
hjh010806 <010806hjh@naver.com>
2024-04-30 15:05:11 +0900

    profile

--- src/main/java/com/example/Project/User/UserRepository.java
+++ src/main/java/com/example/Project/User/UserRepository.java
@@ -2,10 +2,23 @@ package com.example.Project.User;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<SiteUser, Long> {
     SiteUser findByEmail(String email);
 
     SiteUser findByName(String name);
-
+    Optional<SiteUser> findById(Long id);
+@Query("select u " +
+        "from SiteUser u " +
+        "where " +
+        "  number = :number " +
+        "or " +
+        "  nickName = :nickName " +
+        "or " +
+        "  email = :email ")
+    List<SiteUser> checklist(String number, String nickName, String email);
 }
